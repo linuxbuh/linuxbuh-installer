e1126507fe81b78d69384c6a6e9a4894260bf335
linuxbuh <linuxbuh@yandex.ru>
2017-12-19 23:16:14 +0300

    2017-12-19-23-16-14

--- gitadd_and_ftp.sh
+++ gitadd_and_ftp.sh
@@ -17,5 +17,6 @@ git commit -m $tmp
 git push
 
 tar -cvzf /tmp/linuxbuh-installer-"$tmp".tar.gz ./*.* &&
-wput -nd /tmp/linuxbuh-installer-"$tmp".tar.gz ftp://ftpztime:Rfnztdfrfpzdrb1@ftp.linuxbuh.ru/pub/linuxbuh/
+cd /tmp &&
+wput -nd ./linuxbuh-installer-"$tmp".tar.gz ftp://ftpztime:Rfnztdfrfpzdrb1@ftp.linuxbuh.ru/pub/linuxbuh/
 rm -f /tmp/gitadd_and_ftp.tmp
