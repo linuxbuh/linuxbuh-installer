81f3681d2101bfdb2835569571aceef93c1aa34e
linuxbuh <linuxbuh@yandex.ru>
2017-12-19 23:59:49 +0300

    Revision 19

--- add-git-ftp.sh
+++ add-git-ftp.sh
@@ -10,13 +10,23 @@ tmp=`cat /tmp/add-git-ftp.tmp`
 
 echo "Дата $tmp"
 
-git add . &&
 
-git commit -m $tmp &&
+cd /tmp
 
-git push &&
+git clone git@github.com:linuxbuh/linuxbuh-installer.git
 
-tar -cvzf /tmp/linuxbuh-installer-"$tmp".tar.gz ./*.* &&
-cd /tmp &&
-wput -nd ./linuxbuh-installer-"$tmp".tar.gz ftp://ftpztime:Rfnztdfrfpzdrb1@ftp.linuxbuh.ru/pub/linuxbuh/ &&
-rm -f /tmp/add-git-ftp.tmp
+#git add . &&
+
+#git commit -m $tmp &&
+
+#git push &&
+
+#tar -cvzf /tmp/linuxbuh-installer-"$tmp".tar.gz ./*.* &&
+#cd /tmp &&
+#wput -nd ./linuxbuh-installer-"$tmp".tar.gz ftp://ftpztime:Rfnztdfrfpzdrb1@ftp.linuxbuh.ru/pub/linuxbuh/ &&
+
+#a=`ls ./ | grep *.ebuild`   # В переменную 'a' попадает результат работы команды 'echo'
+#echo $a
+
+
+#rm -f /tmp/add-git-ftp.tmp
